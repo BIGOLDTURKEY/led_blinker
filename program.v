f3098e418adeb35fe4a21a92846a573630d1fc32
BIGOLDTURKEY <zaklotfi@outlook.com>
2025-09-22 14:17:15 -0400

    if statements for different frequencies

--- program.v
+++ program.v
@@ -12,11 +12,12 @@ module led_blinker(
 //1  1  1  1
 
 //keep in mind below 50% duty cycle!
+//keep in mind count starts at 0 so do minus 1!
 //25MHz = 25,000,000Hz
-//100Hz --> count=125,000
-//50Hz --> count=250,000
-//10Hz --> count=1,250,000
-//1Hz --> count=12,500,000
+//100Hz --> count=125,000-1
+//50Hz --> count=250,000-1
+//10Hz --> count=1,250,000-1
+//1Hz --> count=12,500,000-1
 
 //log2(25MHz) --> 25 bits
 reg[24:0] count = 0;
@@ -26,17 +27,38 @@ reg t50 = 1'b0;
 reg t10 = 1'b0;
 reg t1 = 1'b0;
 
+//100hz
 always @(posedge i_clock) begin
-        
+    if (count%(125000-1)==0) begin
+        t100 = !t100;
+    end if (!i_switch_1&!i_switch_2) begin
+        o_led_drive = t100;
+    end
+end
+
+//50hz
+always @(posedge i_clock) begin
+    if (count%(250000-1)==0) begin
+        t50 = !t50;
+    end if (!i_switch_1&i_switch_2) begin
+        o_led_drive = t50;
+    end
+end
+
+//10hz
+always @(posedge i_clock) begin
+    if (count%(1250000-1)==0) begin
+        t10 = !t10;
+    end if (i_switch_1&!i_switch_2) begin
+        o_led_drive = t10;
+    end
 end
 always @(posedge i_clock) begin
-    
-end
-always @(posedge i_clock) begin
-    
-end
-always @(posedge i_clock) begin
-    
+    if (count%(12500000-1)==0) begin
+        t1 = !t1;
+    end if (!i_switch_1&!i_switch_2) begin
+        o_led_drive = t1;
+    end
 end
 
 endmodule
